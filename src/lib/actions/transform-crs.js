851bc8335d89ddc163185f4b5f9f582ada5a1f32
Giles Dring <giles.dring@odileeds.org>
2019-11-08 11:43:39 +0000

    Refactoring CRS transformation

--- src/lib/actions/transform-crs.js
+++ src/lib/actions/transform-crs.js
@@ -1,31 +1,35 @@
 import getConversion from '../crs/convert.js';
 
+function convertFromCrs(data) {
+  const { crs: { properties: { name } } } = data;
+  const converter = getConversion(name);
+
+  const convertCoordinates = (coordinate) => {
+    if (!Array.isArray(coordinate)) {
+      throw new Error('Not an array');
+    }
+    if (Array.isArray(coordinate[0])) {
+      return coordinate.map(convertCoordinates);
+    } 
+    return converter(coordinate);
+  };
+
+  const convert = (data) => {
+    const { features, geometry, coordinates } = data;
+    if (features) data.features = features.map(convert);
+    if (geometry) data.geometry = convert(geometry);
+    if (coordinates) data.coordinates = convertCoordinates(coordinates);
+    return data;
+  };
+
+  return convert(data);
+}
+
 export default function testForCrs(data) {
   const { crs } = data;
 
   if (crs) {
-    const { properties: { name } } = crs;
-    const converter = getConversion(name);
-
-    const convertCoordinates = (coordinate) => {
-      if (!Array.isArray(coordinate)) {
-        throw new Error('Not an array');
-      }
-      if (Array.isArray(coordinate[0])) {
-        return coordinate.map(convertCoordinates);
-      } 
-      return converter(coordinate);
-    };
-  
-    const convert = (data) => {
-      const { features, geometry, coordinates } = data;
-      if (features) data.features = features.map(convert);
-      if (geometry) data.geometry = convert(geometry);
-      if (coordinates) data.coordinates = convertCoordinates(coordinates);
-      return data;
-    };
-
-    const convertedData = convert(data);
+    const convertedData = convertFromCrs(data);
     delete convertedData.crs;
     return convertedData;
   } else {
